94b05657d9e99f9e1359a6a1e750291f0810cc7d
satyawaniaman <amansatyawani10@gmail.com>
2025-09-28 13:40:17 +0530

    script added

--- SSR/App.js
+++ SSR/App.js
@@ -1,11 +1,13 @@
-import { createElement as h } from "react";
+import { createElement as h, useState } from "react";
+
 function App() {
   const [count, setCount] = useState(0);
   return h(
     "div",
     null,
-    h("h1", null, "Count: ", count),
+    h("h1", null, "SSR example"),
     h("button", { onClick: () => setCount(count + 1) }, `Count: ${count}`),
   );
 }
+
 export default App;
